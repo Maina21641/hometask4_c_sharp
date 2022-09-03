3d343d633fc73550d21c925c662f9811d9ae423e
Alex <Yulien.ru@Yandex.ru>
2022-09-04 03:30:58 +0400

    added массив из 8 элементов

---
+++ task29/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+int[] RandomArray()
+{
+    int[] array = new int[8];
+    for (int i = 0; i < 8; i ++)
+    {
+        array[i] = new Random().Next(0, 34);
+    }
+    return array;
+}
+    System.Console.Write($"[{string.Join(", ", RandomArray())}]");
